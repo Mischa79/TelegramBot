4960ae92eb1644e51087089c711bd5cfd409aef2
Mischa79 <magaleas79@gmail.com>
2024-05-05 16:11:19 +0200

    kater

--- src/main/java/com/javarush/telegrambot/TelegramBotContent.java
+++ src/main/java/com/javarush/telegrambot/TelegramBotContent.java
@@ -2,78 +2,78 @@ package com.javarush.telegrambot;
 
 public class TelegramBotContent {
     public static final String STEP_1_TEXT = """
-            *ПРИВЕТСТВУЕМ НА ПЕРВОМ УРОВНЕ!*
-                                                                  
-            Ты просыпаешься и понимаешь, что ты — кот. Утро доброе, но что это за удивительные чувства? Твой живот бурчит от сильного голода. Еды в квартире нет, разве что в закрытом холодильнике. Что же делать?
-            
-            Прийдется вспомнить, как хакать цифровой холодильник. Открываем книжку "Продвинутый хакинг для Котов":
-            
-                1. Выполни первое задание, а потом переверни страницу!
-                2. Прокачайся и переходи к самому интересному!
-                3. Сделай дела, чтобы заслужить уважение среди всех местных котов!
-            
-            *Кот домашний обыкновенус, уровень первый.*
-            Взломай цифровой холодильник и выполни первое задание!
-            """;
+             *WILLKOMMEN AUF DER ERSTEN EBENE!*
+                                                                   
+            Du wachst auf und erkennst, dass du eine Katze bist. Guten Morgen, aber was sind das für tolle Gefühle? Dein Magen knurrt vor lauter Hunger. In der Wohnung gibt es keine Lebensmittel, außer im geschlossenen Kühlschrank. Was zu tun?
+             
+             Sie müssen sich merken, wie man einen digitalen Kühlschrank hackt. Öffnen Sie das Buch „Advanced Hacking for Cats“:
+                                                                           
+             1. Erledige die erste Aufgabe und blättere dann um!
+             2. Steigen Sie auf und machen Sie mit dem Spaß weiter!
+             3. Tun Sie Dinge, um sich bei allen einheimischen Katzen Respekt zu verschaffen!
+                                                                           
+             *Normalerweise Hauskatze, Stufe eins.*
+             Hacken Sie den digitalen Kühlschrank und erledigen Sie die erste Aufgabe!
+             """;
 
     public static final String STEP_2_TEXT = """
-            *ПРИВЕТСТВУЕМ НА ВТОРОМ УРОВНЕ!*
+            *WILLKOMMEN AUF DER ZWEITEN EBENE!*
                                   
-            Юху! Ты одолел холодильник. Ты получаешь +20 славы!
-            
-            _Накоплено: 20 славы._
-            
-            Выбирай свою награду:
+            Juhu! Du hast den Kühlschrank erobert. Du erhältst +20 Ruhm!
+                        
+            _Kumuliert: 20 Ruhm._
+                        
+            Wählen Sie Ihre Belohnung:
             """;
 
     public static final String STEP_3_TEXT = """
-            *Кот Айтишник, уровень второй.*
-            Неожиданно, из-за угла выполз жужжащий робот-пылесос. Настало время мести!
-            Виполни второе задание и взломай его!
+            *Cat-IT-Spezialist, Stufe zwei.*
+            Plötzlich kroch um die Ecke ein summender Roboterstaubsauger hervor. Es ist Zeit für Rache!
+            Schließe die zweite Aufgabe ab und hacke sie!
             """;
 
     public static final String STEP_4_TEXT = """
-            *ПРИВЕТСТВУЕМ НА ТРЕТЬЕМ УРОВНЕ!*
+            *WILLKOMMEN AUF LEVEL DRITTE!*
                                     
-            Ты справился с робопылесосом! Ты получаешь +30 славы!
+            Sie beherrschen den Roboterstaubsauger! Du erhältst +30 Ruhm!
                                       
-            _Накоплено: 70 славы._
-            
-            Выбирай свою награду:
+            _Kumuliert: 70 Ruhm._
+                        
+            Wählen Sie Ihre Belohnung:
             """;
 
     public static final String STEP_5_TEXT = """
-            *Кот Робоукротитель, уровень третий.*
-            Хо-хо! На шкафу обнаружен ничейный GoPro!
-            Выполни третье задание — надень и включи его!
+            *Cat Robo Tamer, Level drei.*
+            Ho-ho! Niemandes GoPro wurde im Schrank gefunden!
+            Erledigen Sie die dritte Aufgabe – setzen Sie es auf und schalten Sie es ein!
             """;
 
     public static final String STEP_6_TEXT = """
-            *ПРИВЕТСТВУЕМ НА ЧЕТВЕРТОМ УРОВНЕ!*
+            *WILLKOMMEN AUF STUFE VIER!*
                                     
-            Ты надел GoPro! Ты получаешь +40 славы!
+            Du trägst eine GoPro! Du erhältst +40 Ruhm!
                                     
-            _Накоплено: 140 славы._
-            
-            Выбирай свою награду:
+            _Kumuliert: 140 Ruhm._
+                        
+            Wählen Sie Ihre Belohnung:
             """;
 
     public static final String STEP_7_TEXT = """
-            *Кот Видеоблогер, уровень четвертый.*
-            Теперь отснятый материал нужно залить на компьютер.
-            Выполни четвертое задание — взломай пароль от компьютера!
+            *Katzen-Videoblogger, Stufe vier.*
+            Jetzt muss das Filmmaterial auf den Computer hochgeladen werden.
+            Schließen Sie die vierte Aufgabe ab – knacken Sie das Computerpasswort!
             """;
 
     public static final String STEP_8_TEXT = """
-            *ПРИВЕТСТВУЕМ НА ПЯТОМ УРОВНЕ!*
-            
-            Ты одолел компьютер! Ты получаешь +50 славы!
-            
-            _Накоплено: 230 славы._
+            *WILLKOMMEN AUF STUFE FÜNF!*
+                        
+            Du hast den Computer besiegt! Du erhältst +50 Ruhm!
+                        
+            _Kumuliert: 230 Ruhm._
             """;
 
     public static final String FINAL_TEXT = """
-            *Кот Хакер, уровень пятый.*
-            День прошел не зря: кот-хакер выполнил свою миссию и заслужил уважение среди местных дворовых котов.
+            *Cat Hacker, Level fünf.*
+            Der Tag war nicht umsonst: Die Hackerkatze hat ihre Mission erfüllt und sich den Respekt der örtlichen Hofkatzen erworben.
             """;
 }
